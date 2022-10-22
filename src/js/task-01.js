43d855c2c00af34f1fc540b8a7f0681ab48844f6
Katerina Dmytrenko <107727106+katerinadmit@users.noreply.github.com>
2022-10-22 13:43:31 +0300

    1-2

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,7 +1,21 @@
-const allItems = document.querySelectorAll('.item');
-const allItemsQty = allItems.length;
-
-console.log(`Number of categories: ${allItemsQty} `);
+const allItem = document.querySelectorAll('.item');
+console.log(`Number of categories:` ,allItem.length);
 
 
+allItem.forEach(category => {
+    console.log(
+        `Category:`,
+      category.firstElementChild.textContent,
+      `Elements:`,
+      category.lastElementChild.children.length,
+    );
+  });
+
+//   const allItem = document.querySelectorAll('.item');
+//   console.log(`Number of categories: ${allItem.length}`)
+  
+//   allItem.forEach(item => {
+//       console.log(`Category: ${item.firstElementChild.textContent}`);
+//       console.log(`Elements: ${item.lastElementChild.children.length}`)
+//   });
 
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const listOfEl = document.querySelector('#ingredients')
+
+const listEl = ingredients.map(ingredient => {
+
+  const itemEl = document.createElement('li')
+  itemEl.textContent = ingredient;
+itemEl.classList.add('item')  
+  return itemEl
+});
+
+listOfEl.append(...listEl)
+console.log(listOfEl)
