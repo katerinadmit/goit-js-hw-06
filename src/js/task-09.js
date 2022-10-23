e0f281277e8a267d2dd1e8d56580b71ad6141c5e
Katerina Dmytrenko <107727106+katerinadmit@users.noreply.github.com>
2022-10-23 23:03:35 +0300

    9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl= document.querySelector('.body-color');
+const btn = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+console.log(bodyEl)
+console.log(btn)
+
+btn.addEventListener('click',onBtnClick)
+
+function onBtnClick() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorSpan.textContent = bodyEl.style.backgroundColor
+}
+
